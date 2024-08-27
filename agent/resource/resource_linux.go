504c036a0e0ddc76771d50d4514a8c99569e5785
Your Name <you@example.com>
2024-08-27 15:04:43 +0800

    add metrics

--- agent/resource/resource_linux.go
+++ agent/resource/resource_linux.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"golang.org/x/sys/unix"
 )
@@ -57,4 +58,103 @@ func GetGateway() string {
 		}
 	}
 	return ""
+}
+func GetTCPCount() int {
+	var count int
+	for _, tcpFile := range []string{"/proc/net/tcp", "/proc/net/tcp6"} {
+		tcpData, err := os.ReadFile(tcpFile)
+		if err == nil {
+			tcps := bytes.Split(tcpData, []byte("\n"))
+			count = len(tcps)
+			if count > 1 {
+				count--
+			}
+		}
+	}
+	return count
+}
+
+type ReceiveBytes uint64
+type TransmitBytes uint64
+
+func UploadDownloadFlow(dev string) (DownloadFlow, UploadFlow, error) {
+	down, up, err := TotalFlowByDevice(dev)
+	if err != nil {
+		return "", "", err
+	}
+	time.Sleep(time.Second * 1)
+
+	down2, up2, err := TotalFlowByDevice(dev)
+	if err != nil {
+		return "", "", err
+	}
+
+	downStr := int64(down2 - down)
+	upStr := int64(up2 - up)
+
+	return DownloadFlow(downStr), UploadFlow(upStr), nil
+}
+
+func TotalFlowByDevice(dev string) (ReceiveBytes, TransmitBytes, error) {
+	devInfo, err := os.ReadFile("/proc/net/dev")
+	if err != nil {
+		return 0, 0, err
+	}
+
+	var receive int = -1
+	var transmit int = -1
+
+	var receiveBytes uint64
+	var transmitBytes uint64
+
+	lines := strings.Split(string(devInfo), "\n")
+	for _, line := range lines {
+		if strings.Contains(line, dev) {
+			i := 0
+			fields := strings.Split(line, ":")
+			for _, field := range fields {
+				if strings.Contains(field, dev) {
+					i = 1
+				} else {
+					values := strings.Fields(field)
+					for _, value := range values {
+						//logger.Debug(value)
+						if receive == i {
+							bytes, _ := strconv.ParseInt(value, 10, 64)
+							receiveBytes = uint64(bytes)
+						} else if transmit == i {
+							bytes, _ := strconv.ParseInt(value, 10, 64)
+							transmitBytes = uint64(bytes)
+						}
+						i++
+					}
+				}
+			}
+		} else if strings.Contains(line, "face") {
+			index := 0
+			tag := false
+			fields := strings.Split(line, "|")
+			for _, field := range fields {
+				if strings.Contains(field, "face") {
+					index = 1
+				} else if strings.Contains(field, "bytes") {
+					values := strings.Fields(field)
+					for _, value := range values {
+						//logger.Debug(value)
+						if strings.Contains(value, "bytes") {
+							if !tag {
+								tag = true
+								receive = index
+							} else {
+								transmit = index
+							}
+						}
+						index++
+					}
+				}
+			}
+		}
+	}
+
+	return ReceiveBytes(receiveBytes), TransmitBytes(transmitBytes), nil
 }
